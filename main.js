2f2e7d00d34ad822c4c90e03bf408a418284d88e
ul00lu <serafim.kara08@mail.ru>
2024-10-16 19:35:50 +0600

    zzedfweffe

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const words = ['работают', 'вдохновляют', 'удивляют', 'развиваются']
-	const span = document.querySelector('.phases') 
+	const span = document.querySelector('.phases')
 	let wordIndex = 0
-	const changeTime = 3000 
+	const changeTime = 3000
 	function changeWord() {
-		
 		span.classList.add('fade-out')
 
-	
 		setTimeout(() => {
-			wordIndex = (wordIndex + 1) % words.length 
-			span.textContent = words[wordIndex] 
+			wordIndex = (wordIndex + 1) % words.length
+			span.textContent = words[wordIndex]
 
-			
 			span.classList.remove('fade-out')
-		}, 600) 
+		}, 600)
 	}
 
 	setInterval(changeWord, changeTime)
